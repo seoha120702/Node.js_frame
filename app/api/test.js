6ce4386623a1f5952de20807a789a4daba69bc76
seoha120702 <c29547416@gmail.com>
2024-02-24 11:16:58 +0900

    ss

--- app/api/test.js
+++ app/api/test.js
@@ -1,5 +1,8 @@
+//see "./router.ctrl.js"
+const ctrl = require("./router.ctrl");
+
 router = (req, res) => {
-    res.status(200).send("testestest");
+    res.send("hhhhh")
 };
 
 module.exports = router
